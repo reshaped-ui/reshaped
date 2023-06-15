62056f4c49eda23cbf4961b18ff543afbdfc8f5d
Dmitry Belyaev <dmitry.belyaev@booking.com>
2023-06-15 23:35:05 +0200

    Migrated build to vite

--- src/index.ts
+++ src/index.ts
@@ -1,56 +1,144 @@
 /**
  * Components
  */
-export { default as Accordion, AccordionProps } from "components/Accordion";
-export { default as Actionable, ActionableProps, ActionableRef } from "components/Actionable";
-export { default as ActionBar, ActionBarProps } from "components/ActionBar";
-export { default as Alert, AlertProps } from "components/Alert";
-export { default as Avatar, AvatarProps } from "components/Avatar";
-export { default as Badge, BadgeProps } from "components/Badge";
-export { default as Button, ButtonProps, ButtonAlignerProps } from "components/Button";
-export { default as Breadcrumbs, BreadcrumbsProps } from "components/Breadcrumbs";
-export { default as Card, CardProps } from "components/Card";
-export { default as Carousel, CarouselProps, CarouselInstanceRef } from "components/Carousel";
-export { default as Checkbox, CheckboxProps } from "components/Checkbox";
-export { default as CheckboxGroup, CheckboxGroupProps } from "components/CheckboxGroup";
-export { default as Container, ContainerProps } from "components/Container";
-export { default as Dismissible, DismissibleProps } from "components/Dismissible";
-export { default as Divider, DividerProps } from "components/Divider";
-export { default as DropdownMenu, DropdownMenuProps } from "components/DropdownMenu";
-export { default as FormControl, FormControlProps } from "components/FormControl";
-export { default as Hidden, HiddenProps } from "components/Hidden";
-export { default as HiddenVisually, HiddenVisuallyProps } from "components/HiddenVisually";
-export { default as Hotkey, HotkeyProps } from "components/Hotkey";
-export { default as Icon, IconProps } from "components/Icon";
-export { default as Image, ImageProps } from "components/Image";
-export { default as Link, LinkProps } from "components/Link";
-export { default as Loader, LoaderProps } from "components/Loader";
-export { default as MenuItem, MenuItemProps } from "components/MenuItem";
-export { default as Modal, ModalProps } from "components/Modal";
-export { default as Overlay, OverlayProps } from "components/Overlay";
-export { default as Popover, PopoverProps } from "components/Popover";
-export { default as Progress, ProgressProps } from "components/Progress";
-export { default as Radio, RadioProps } from "components/Radio";
-export { default as RadioGroup, RadioGroupProps } from "components/RadioGroup";
-export { default as Reshaped, ReshapedProps } from "components/Reshaped";
-export { default as Scrim, ScrimProps } from "components/Scrim";
-export { default as Select, SelectProps } from "components/Select";
-export { default as Skeleton, SkeletonProps } from "components/Skeleton";
-export { default as Switch, SwitchProps } from "components/Switch";
-export { default as Tabs, TabsProps } from "components/Tabs";
-export { default as Text, TextProps } from "components/Text";
-export { default as TextArea, TextAreaProps } from "components/TextArea";
-export { default as TextField, TextFieldProps } from "components/TextField";
-export { default as Timeline, TimelineProps, TimelineItemProps } from "components/Timeline";
-export { useToast, ToastProps } from "components/Toast";
-export { default as Tooltip, TooltipProps } from "components/Tooltip";
-export { default as View, ViewProps, ViewItemProps } from "components/View";
+export { default as Accordion } from "components/Accordion";
+export type { AccordionProps } from "components/Accordion";
+
+export { default as Actionable } from "components/Actionable";
+export type { ActionableProps, ActionableRef } from "components/Actionable";
+
+export { default as ActionBar } from "components/ActionBar";
+export type { ActionBarProps } from "components/ActionBar";
+
+export { default as Alert } from "components/Alert";
+export type { AlertProps } from "components/Alert";
+
+export { default as Avatar } from "components/Avatar";
+export type { AvatarProps } from "components/Avatar";
+
+export { default as Badge } from "components/Badge";
+export type { BadgeProps } from "components/Badge";
+
+export { default as Button } from "components/Button";
+export type { ButtonProps, ButtonAlignerProps } from "components/Button";
+
+export { default as Breadcrumbs } from "components/Breadcrumbs";
+export type { BreadcrumbsProps } from "components/Breadcrumbs";
+
+export { default as Card } from "components/Card";
+export type { CardProps } from "components/Card";
+
+export { default as Carousel } from "components/Carousel";
+export type { CarouselProps, CarouselInstanceRef } from "components/Carousel";
+
+export { default as Checkbox } from "components/Checkbox";
+export type { CheckboxProps } from "components/Checkbox";
+
+export { default as CheckboxGroup } from "components/CheckboxGroup";
+export type { CheckboxGroupProps } from "components/CheckboxGroup";
+
+export { default as Container } from "components/Container";
+export type { ContainerProps } from "components/Container";
+
+export { default as Dismissible } from "components/Dismissible";
+export type { DismissibleProps } from "components/Dismissible";
+
+export { default as Divider } from "components/Divider";
+export type { DividerProps } from "components/Divider";
+
+export { default as DropdownMenu } from "components/DropdownMenu";
+export type { DropdownMenuProps } from "components/DropdownMenu";
+
+export { default as FormControl } from "components/FormControl";
+export type { FormControlProps } from "components/FormControl";
+
+export { default as Hidden } from "components/Hidden";
+export type { HiddenProps } from "components/Hidden";
+
+export { default as HiddenVisually } from "components/HiddenVisually";
+export type { HiddenVisuallyProps } from "components/HiddenVisually";
+
+export { default as Hotkey } from "components/Hotkey";
+export type { HotkeyProps } from "components/Hotkey";
+
+export { default as Icon } from "components/Icon";
+export type { IconProps } from "components/Icon";
+
+export { default as Image } from "components/Image";
+export type { ImageProps } from "components/Image";
+
+export { default as Link } from "components/Link";
+export type { LinkProps } from "components/Link";
+
+export { default as Loader } from "components/Loader";
+export type { LoaderProps } from "components/Loader";
+
+export { default as MenuItem } from "components/MenuItem";
+export type { MenuItemProps } from "components/MenuItem";
+
+export { default as Modal } from "components/Modal";
+export type { ModalProps } from "components/Modal";
+
+export { default as Overlay } from "components/Overlay";
+export type { OverlayProps } from "components/Overlay";
+
+export { default as Popover } from "components/Popover";
+export type { PopoverProps } from "components/Popover";
+
+export { default as Progress } from "components/Progress";
+export type { ProgressProps } from "components/Progress";
+
+export { default as Radio } from "components/Radio";
+export type { RadioProps } from "components/Radio";
+
+export { default as RadioGroup } from "components/RadioGroup";
+export type { RadioGroupProps } from "components/RadioGroup";
+
+export { default as Reshaped } from "components/Reshaped";
+export type { ReshapedProps } from "components/Reshaped";
+
+export { default as Scrim } from "components/Scrim";
+export type { ScrimProps } from "components/Scrim";
+
+export { default as Select } from "components/Select";
+export type { SelectProps } from "components/Select";
+
+export { default as Skeleton } from "components/Skeleton";
+export type { SkeletonProps } from "components/Skeleton";
+
+export { default as Switch } from "components/Switch";
+export type { SwitchProps } from "components/Switch";
+
+export { default as Tabs } from "components/Tabs";
+export type { TabsProps } from "components/Tabs";
+
+export { default as Text } from "components/Text";
+export type { TextProps } from "components/Text";
+
+export { default as TextArea } from "components/TextArea";
+export type { TextAreaProps } from "components/TextArea";
+
+export { default as TextField } from "components/TextField";
+export type { TextFieldProps } from "components/TextField";
+
+export { default as Timeline } from "components/Timeline";
+export type { TimelineProps, TimelineItemProps } from "components/Timeline";
+
+export { useToast } from "components/Toast";
+export type { ToastProps } from "components/Toast";
+
+export { default as Tooltip } from "components/Tooltip";
+export type { TooltipProps } from "components/Tooltip";
+
+export { default as View } from "components/View";
+export type { ViewProps, ViewItemProps } from "components/View";
 
 /**
  * Hooks
  */
 export { useFormControl } from "components/FormControl";
-export { default as Theme, useTheme, ThemeProps } from "components/Theme";
+export { default as Theme, useTheme } from "components/Theme";
+export type { ThemeProps } from "components/Theme";
 export { default as useScrollLock } from "hooks/useScrollLock";
 export { default as useToggle } from "hooks/useToggle";
 export { default as useRTL } from "hooks/useRTL";
@@ -66,8 +154,8 @@ export { classNames, responsiveClassNames, responsivePropDependency } from "util
 /**
  * Types
  */
-export { ReshapedConfig } from "types/config";
-export { Responsive } from "types/global";
+export type { ReshapedConfig } from "types/config";
+export type { Responsive } from "types/global";
 
 /**
  * Dev utilities
