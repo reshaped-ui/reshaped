3b31463982625ff670eac811f0c7e148783523b5
Dmitry Belyaev <blv.dmitry@gmail.com>
2023-08-06 22:49:27 +0200

    Commented next config

--- src/config/next.ts
+++ src/config/next.ts
@@ -1,5 +1,24 @@
-export const config = {
-	modularizeImports: {
-		reshaped: [[""]],
-	},
-};
+/**
+ * TODO: Work in progress
+ * Needs named exports support before we can use it
+ */
+
+// export const config = {
+// 	modularizeImports: {
+// 		reshaped: {
+// 			transform: {
+// 				// Utilities
+// 				// classNames: "reshaped/utilities/classNames",
+
+// 				// Hooks
+// 				useToast: "reshaped/components/Toast",
+// 				useFormControl: "reshaped/components/FormControl",
+// 				useTheme: "reshaped/components/Theme",
+// 				"use.*": "reshaped/hooks/{{ member }}",
+
+// 				// Components
+// 				"*": "reshaped/components/{{ member }}",
+// 			},
+// 		},
+// 	},
+// };
