63723c30adf18ad8a49b8afc2196c344204d8aba
Dmitry Belyaev <blv.dmitry@gmail.com>
2023-10-31 23:57:28 +0100

    chore: updated release setup

--- release.config.js
+++ release.config.js
@@ -17,7 +17,7 @@ module.exports = {
 			"@semantic-release/git",
 			{
 				assets: ["CHANGELOG.md", "package.json"],
-				message: "chore(global): publish",
+				message: "chore: publish",
 			},
 		],
 	],
