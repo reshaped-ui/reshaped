308086b8c595bea646bf9474e90f4458c6002c5d
Dmitry Belyaev <blv.dmitry@gmail.com>
2023-12-29 22:57:16 +0100

    feat: added getConfig function for postcss setup

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,6 +2,7 @@
 
 - Theming: Warning colors
 - Theming: Brand color
+- Postcss: Added getConfig() + with custom-media option
 
 - Overlay: Fixed tailwind default cursor
 - Icon: Fixed svg rendering defined in components with prop types
@@ -10,3 +11,6 @@
 - Badge, Figma: Exposed icon properties
 - Button, Figma: Split into Button and .button-content to improve the library
 - Button: Added slot support
+
+- Timeline: Fixed slot auto layout in Figma
+- Slot: Fixed slot overriding styles of the swapped components
--- src/config/postcss.ts
+++ src/config/postcss.ts
@@ -1,10 +1,30 @@
+import path from "path";
+
+const defaultThemeMediaCSSPath = path.resolve(__dirname, "../themes/media.css");
+
 // Using [plugin]: { ...options } format here because it's supported by the most frameworks
 // - require('plugin') is not supported by Next
 // - ['plugin', options] is not supported by Vite
 export const config = {
 	plugins: {
-		"postcss-custom-media": {},
+		"postcss-custom-media": {
+			importFrom: defaultThemeMediaCSSPath,
+		},
 		autoprefixer: {},
 		cssnano: { preset: ["default", { calc: false }] },
 	},
+};
+
+export const getConfig = (options: { themeMediaCSSPath: string }) => {
+	const { themeMediaCSSPath = defaultThemeMediaCSSPath } = options;
+
+	return {
+		plugins: {
+			"postcss-custom-media": {
+				importFrom: themeMediaCSSPath,
+			},
+			autoprefixer: {},
+			cssnano: { preset: ["default", { calc: false }] },
+		},
+	};
 };
