14248d212588bc55a329706d022f721d84d1f512
Dmitry Belyaev <blv.dmitry@gmail.com>
2024-04-03 22:25:16 +0200

    chore: updated npm package license

--- bin/release-source.sh
+++ bin/release-source.sh
@@ -1,7 +1,20 @@
-mv LICENSE.md LICENSE-TEMP.md
-mv LICENSE-SOURCE.md LICENSE.md
+LICENSE_PATH="LICENSE.md"
+LICENSE_SOURCE_PATH="LICENSE-SOURCE.md"
+LICENSE_TMP_PATH="LICENSE-TEMP.md"
+
+PKG_JSON_PATH="./package.json"
+PKG_JSON_TMP_PATH="./package-temp.json"
+
+
+mv $LICENSE_PATH $LICENSE_TMP_PATH
+mv $LICENSE_SOURCE_PATH $LICENSE_PATH
+
+cp $PKG_JSON_PATH $PKG_JSON_TMP_PATH
+jq --arg license "SEE LICENSE IN LICENSE.md" '.license = $license' $PKG_JSON_PATH > ./temp.json && mv temp.json $PKG_JSON_PATH
 
 git archive -o reshaped-source-v$(jq -r .version package.json).zip HEAD . ':!.chromatic'
 
-mv LICENSE.md LICENSE-SOURCE.md
-mv LICENSE-TEMP.md LICENSE.md
+mv $LICENSE_PATH $LICENSE_SOURCE_PATH
+mv $LICENSE_TMP_PATH $LICENSE_PATH
+
+mv $PKG_JSON_TMP_PATH $PKG_JSON_PATH
