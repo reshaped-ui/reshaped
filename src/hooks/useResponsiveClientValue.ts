30dc800b6ad64f92e197064755c2849a57a022f9
Dmitry Belyaev <blv.dmitry@gmail.com>
2024-10-18 21:51:31 +0200

    feat(useResponsiveClientValue): custom viewports support

--- src/hooks/useResponsiveClientValue.ts
+++ src/hooks/useResponsiveClientValue.ts
@@ -3,26 +3,41 @@
 import React from "react";
 import { SingletonEnvironmentContext } from "hooks/_private/useSingletonEnvironment";
 import useIsomorphicLayoutEffect from "hooks/useIsomorphicLayoutEffect";
+import defaultBreakpoints from "constants/breakpoints";
 import type * as G from "types/global";
-
-const breakpoints = {
-	m: 660,
-	l: 900,
-	xl: 1280,
-};
-
-const mediaQueries = {
-	s: `(max-width: ${breakpoints.m - 1}px)`,
-	m: `(min-width: ${breakpoints.m}px) and (max-width: ${breakpoints.l - 1}px)`,
-	l: `(min-width: ${breakpoints.l}px) and (max-width: ${breakpoints.xl - 1}px)`,
-	xl: `(min-width: ${breakpoints.xl}px)`,
-};
 
 const useResponsiveClientValue = <T extends unknown>(value: G.Responsive<T>): T | undefined => {
 	const { defaultViewport } = React.useContext(SingletonEnvironmentContext);
 	const [viewport, setViewport] = React.useState(defaultViewport);
 
 	useIsomorphicLayoutEffect(() => {
+		const rootThemeEl = document.querySelector("[data-rs-theme]");
+		const rootStyle = rootThemeEl && window.getComputedStyle(rootThemeEl);
+
+		/**
+		 * We generate variables for the viewport breakpoints in the themes
+		 * We use them here in case they're custom and fallback to the default values
+		 * in case there is no SSR passing the data-rs-theme attribute
+		 */
+		const breakpoints = {
+			m:
+				(rootStyle && Number(rootStyle.getPropertyValue("--rs-viewport-m-min"))) ||
+				defaultBreakpoints.m,
+			l:
+				(rootStyle && Number(rootStyle.getPropertyValue("--rs-viewport-l-min"))) ||
+				defaultBreakpoints.l,
+			xl:
+				(rootStyle && Number(rootStyle.getPropertyValue("--rs-viewport-xl-min"))) ||
+				defaultBreakpoints.xl,
+		};
+
+		const mediaQueries = {
+			s: `(max-width: ${breakpoints.m - 1}px)`,
+			m: `(min-width: ${breakpoints.m}px) and (max-width: ${breakpoints.l - 1}px)`,
+			l: `(min-width: ${breakpoints.l}px) and (max-width: ${breakpoints.xl - 1}px)`,
+			xl: `(min-width: ${breakpoints.xl}px)`,
+		};
+
 		const viewports = Object.keys(mediaQueries) as (keyof typeof mediaQueries)[];
 		const matchers = viewports.map((viewport) => {
 			const mq = window.matchMedia(mediaQueries[viewport]);
--- src/themes/_generator/tokens/viewport/viewport.transforms.ts
+++ src/themes/_generator/tokens/viewport/viewport.transforms.ts
@@ -1,5 +1,5 @@
 import type * as T from "./viewport.types";
-import type { Transformer } from "../types";
+import type { TransformedToken, Transformer } from "../types";
 
 const transformedToken: Transformer<T.Token | T.SToken> = (name, token) => {
 	const value: string[] = [];
@@ -7,7 +7,7 @@ const transformedToken: Transformer<T.Token | T.SToken> = (name, token) => {
 	if (token.minPx) value.push(`(min-width: ${token.minPx}px)`);
 	if (token.maxPx) value.push(`(max-width: ${token.maxPx}px)`);
 
-	return [
+	const result: TransformedToken[] = [
 		{
 			name,
 			tokenType: "viewport",
@@ -15,6 +15,17 @@ const transformedToken: Transformer<T.Token | T.SToken> = (name, token) => {
 			value: value.join(" and "),
 		},
 	];
+
+	if (token.minPx) {
+		result.push({
+			name: `${name}-min`,
+			tokenType: "viewport",
+			type: "variable",
+			value: token.minPx.toString(),
+		});
+	}
+
+	return result;
 };
 
 export default transformedToken;
