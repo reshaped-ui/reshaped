e1fe374d29c8c5f3b067628957612b6da5abc15f
Dmitry Belyaev <blv.dmitry@gmail.com>
2024-10-30 23:23:39 +0100

    test(Reshaped): scoped tests

--- src/components/Reshaped/tests/Reshaped.test.tsx
+++ src/components/Reshaped/tests/Reshaped.test.tsx
@@ -2,6 +2,8 @@ import { render, screen } from "@testing-library/react";
 import { userEvent } from "@testing-library/user-event";
 import MatchMediaMock from "jest-matchmedia-mock";
 import Reshaped from "components/Reshaped";
+import { useTheme } from "components/Theme";
+import Button from "components/Button";
 
 // Render is used in every test because otherwise matchMedia mock is failing
 let matchMedia: MatchMediaMock;
@@ -71,5 +73,35 @@ describe("Utilities/Reshaped", () => {
 		expect(rootEl).not.toBe(document.documentElement);
 		expect(rootEl).toHaveAttribute("data-rs-theme", "reshaped");
 		expect(document.documentElement).not.toHaveAttribute("data-rs-theme");
+	});
+
+	test("switched color mode only for the scoped nested provider", async () => {
+		const Component = () => {
+			const { invertColorMode } = useTheme();
+
+			return <Button onClick={invertColorMode} />;
+		};
+
+		render(
+			<Reshaped theme="reshaped">
+				<Reshaped theme="slate" scoped>
+					<Component />
+				</Reshaped>
+			</Reshaped>
+		);
+
+		const nestedRoot = document.querySelector("[data-rs-root]");
+		const button = screen.getByRole("button");
+
+		expect(document.documentElement).toHaveAttribute("data-rs-theme", "reshaped");
+		expect(document.documentElement).toHaveAttribute("data-rs-color-mode", "light");
+
+		expect(nestedRoot).toHaveAttribute("data-rs-theme", "slate");
+		expect(nestedRoot).toHaveAttribute("data-rs-color-mode", "light");
+
+		await userEvent.click(button);
+
+		expect(document.documentElement).toHaveAttribute("data-rs-color-mode", "light");
+		expect(nestedRoot).toHaveAttribute("data-rs-color-mode", "dark");
 	});
 });
