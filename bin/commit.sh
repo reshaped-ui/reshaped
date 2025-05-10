b4ed5bd9d95e28a9ff3a0714b9ec4043626ffa34
Dmitry Belyaev <blv.dmitry@gmail.com>
2025-05-10 17:37:34 +0200

    chore: test changelog

--- bin/commit.sh
+++ bin/commit.sh
@@ -3,6 +3,6 @@
 set -e
 
 yarn git-cz
-yarn conventional-changelog -c ../changelog.config.cjs -i CHANGELOG.md -s 
+yarn conventional-changelog -c ./changelog.config.cjs -i CHANGELOG.md -s 
 git add CHANGELOG.md
 git commit --amend --no-edit
