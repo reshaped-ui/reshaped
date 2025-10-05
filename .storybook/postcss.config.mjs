a4d5ee32059d5400a69de468a2cd243043ff933c
Dmitry Belyaev <blv.dmitry@gmail.com>
2025-10-05 21:15:08 +0200

    chore: storybook config to mjs

--- .storybook/postcss.config.mjs
+++ .storybook/postcss.config.mjs
@@ -1,11 +1,11 @@
-import path, { dirname } from "path";
+import path from "path";
 import { fileURLToPath } from "url";
 import customMediaPlugin from "postcss-custom-media";
 import postcssGlobalData from "@csstools/postcss-global-data";
-import baseConfig from "../tools/build/postcss.config";
+import baseConfig from "../tools/build/postcss.config.mjs";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 export default {
 	plugins: [
