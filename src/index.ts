d8a901884e8c2baa6b9dbbc2250c19d3fa075303
Dmitry Belyaev <blv.dmitry@gmail.com>
2025-10-11 15:35:57 +0200

    feat(useKeyboardArrowNavigation): export

--- src/index.ts
+++ src/index.ts
@@ -208,6 +208,7 @@ export { default as useHandlerRef } from "hooks/useHandlerRef";
 export { default as useHotkeys } from "hooks/useHotkeys";
 export { default as useIsomorphicLayoutEffect } from "hooks/useIsomorphicLayoutEffect";
 export { default as useKeyboardMode } from "hooks/useKeyboardMode";
+export { default as useKeyboardArrowNavigation } from "hooks/useKeyboardArrowNavigation";
 export { default as useOnClickOutside } from "hooks/useOnClickOutside";
 export { default as useResponsiveClientValue } from "hooks/useResponsiveClientValue";
 export { default as useRTL } from "hooks/useRTL";
