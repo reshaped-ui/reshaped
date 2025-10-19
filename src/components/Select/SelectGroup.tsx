4d51dfcc7c488c0a88f1cedc819f01e60a937b1e
Dmitry Belyaev <blv.dmitry@gmail.com>
2025-10-20 00:23:06 +0200

    feat(Select): composition improvements

--- src/components/Select/SelectGroup.tsx
+++ src/components/Select/SelectGroup.tsx
@@ -4,21 +4,23 @@ import Text from "components/Text";
 import type * as T from "./Select.types";
 import s from "./Select.module.css";
 
-const SelectOptionGroup: React.FC<T.OptionGroupProps> = (props) => {
+const SelectGroup: React.FC<T.GroupProps> = (props) => {
 	const { label, children } = props;
 
 	return (
 		<View attributes={{ role: "group" }} gap={1} className={s.group}>
-			<div className={s["group-label"]}>
-				<Text variant="caption-1" color="neutral-faded">
-					{label}
-				</Text>
-			</div>
+			{label && (
+				<div className={s["group-label"]}>
+					<Text variant="caption-1" color="neutral-faded">
+						{label}
+					</Text>
+				</div>
+			)}
 			<View.Item>{children}</View.Item>
 		</View>
 	);
 };
 
-SelectOptionGroup.displayName = "Select.OptionGroup";
+SelectGroup.displayName = "Select.Group";
 
-export default SelectOptionGroup;
+export default SelectGroup;
