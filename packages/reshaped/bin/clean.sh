0ff22dd33bf9231b174292e3e302a31319194d9e
Pavlo <its.monotype@gmail.com>
2026-01-15 01:15:45 +0100

    chore: make build scripts cross-platform (#567)

--- packages/reshaped/bin/clean.sh
+++ packages/reshaped/bin/clean.sh
@@ -1,3 +1,5 @@
+#!/usr/bin/env bash
+
 find ./src -type d -mindepth 1 -maxdepth 1 | xargs -L1 basename | xargs -L1 rm -rf
 rm -rf dist
 rm -f index.js index.d.ts bundle.js bundle.js.map bundle.css bundle.css.map bundle.d.ts
