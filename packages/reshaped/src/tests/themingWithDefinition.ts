dc85dfec8cfa9df882a2a8cb8c25c6d860ff442f
Dmitry Belyaev <blv.dmitry@gmail.com>
2026-03-31 21:28:21 +0200

    v4: test size scripts

--- packages/reshaped/src/tests/themingWithDefinition.ts
+++
@@ -1,6 +0,0 @@
-/**
- * File importing theming client to test its bundle size
- */
-import * as themes from "../themes";
-
-console.log(themes);
--- packages/reshaped/src/tests/themingWithoutDefinition.ts
+++
@@ -1,6 +0,0 @@
-/**
- * File importing theming client to test its bundle size
- */
-import { generateThemeColors, getThemeCSS } from "../themes";
-
-console.log({ getThemeCSS, generateThemeColors });
