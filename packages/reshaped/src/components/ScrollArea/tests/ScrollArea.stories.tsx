28d4edbd4e4e733ff5d6deec0d0e4813c189e742
Dmitry Belyaev <blv.dmitry@gmail.com>
2026-08-15 15:30:16 +0200

    scroll area: styling updates

--- packages/reshaped/src/components/ScrollArea/tests/ScrollArea.stories.tsx
+++ packages/reshaped/src/components/ScrollArea/tests/ScrollArea.stories.tsx
@@ -377,6 +377,23 @@ export const testComposition: StoryObj = {
 						</ScrollArea>
 					</View>
 				</Example.Item>
+
+				<Example.Item title="inside a card with rounded corners">
+					<Card padding={0} height="120px">
+						<ScrollArea scrollbarDisplay="visible">
+							<View padding={4}>
+								Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+								Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+								unknown printer took a galley of type and scrambled it to make a type specimen book.
+								It has survived not only five centuries, but also the leap into electronic
+								typesetting, remaining essentially unchanged. It was popularised in the 1960s with
+								the release of Letraset sheets containing Lorem Ipsum passages, and more recently
+								with desktop publishing software like Aldus PageMaker including versions of Lorem
+								Ipsum.
+							</View>
+						</ScrollArea>
+					</Card>
+				</Example.Item>
 			</Example>
 		);
 	},
